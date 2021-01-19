ac5e6d411e855ae50a18ae5354fc454981168e7e
robbe.luo <robbe.luo@blacklake.cn>
2021-01-19 15:55:37 +0800

    add dialogAlert

--- android/src/main/java/com/robbe/lib/ReactNativeAlertModule.java
+++ android/src/main/java/com/robbe/lib/ReactNativeAlertModule.java
@@ -2,6 +2,7 @@ package com.robbe.lib;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
@@ -9,18 +10,17 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.robbe.lib.interfaces.ReadableCustomMap;
+import com.facebook.react.bridge.ReadableMap;
 
 
 public class ReactNativeAlertModule extends ReactContextBaseJavaModule {
     public static final String REACT_CLASS = "ReactNativeAlert";
-    private AlertDialog dialog;
+    private AlertDialog.Builder builder;
+    private AlertDialog alertDialog;
 
     public ReactNativeAlertModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -32,49 +32,62 @@ public class ReactNativeAlertModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void show(ReadableCustomMap readableMap, Promise promise) {
+    public void show(ReadableMap readableMap, final Callback confirmCallback, final Callback cancelCallback) {
         Activity activity = this.getCurrentActivity();
         String title = readableMap.hasKey("title") ? readableMap.getString("title") : null;
         String message = readableMap.hasKey("message") ? readableMap.getString("message") : null;
         String cancelText = readableMap.hasKey("cancelText") ? readableMap.getString("cancelText") : null;
-        final Callback cancelBack = readableMap.hasKey("cancelBack") ? (Callback) readableMap.getCallback("cancelBack") : null;
+        String okText = readableMap.hasKey("okText") ? readableMap.getString("okText") : null;
+        boolean cancelable = readableMap.hasKey("cancelable") ? readableMap.getBoolean("cancelable") : false;
 
-        if (this.dialog == null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            View view = View.inflate(activity, R.layout.diglog_alert_module, null);
-            builder.setView(view);
-            builder.setCancelable(false);
-
-            if (title != null) {
-                TextView textView = (TextView) view.findViewById(R.id.title);
-                textView.setText(title);
-            }
-
-            Button btnCancel = (Button) view.findViewById(R.id.btn_cancel);
-            if (cancelText != null) {
-                btnCancel.setText(cancelText);
-            }
-            btnCancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (dialog != null) dialog.dismiss();
-                    if (cancelBack != null) cancelBack.invoke();
-                }
-            });
-
+        if (this.builder == null) {
+            this.builder = new AlertDialog.Builder(activity, R.style.ThemeOverlay_AppCompat_MaterialAlertDialog);
             if (message != null) {
-                TextView textView = (TextView) view.findViewById(R.id.text_msg);
-                textView.setText(message);
+                this.builder.setMessage(message);
+            }
+            if (title != null) {
+                View view = View.inflate(activity, R.layout.diglog_alert_module, null);
+                TextView textView = view.findViewById(R.id.title);
+                textView.setText(title);
+                this.builder.setCustomTitle(view);
+            }
+            if (okText != null) {
+                this.builder.setPositiveButton(okText, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (confirmCallback != null) {
+                            confirmCallback.invoke();
+                        }
+                    }
+                });
             }
 
+            if (cancelCallback != null) {
+                this.builder.setNegativeButton(cancelText, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (cancelCallback != null) {
+                            cancelCallback.invoke();
+                        }
+                    }
+                });
+            }
 
-            this.dialog = builder.create();
-            this.dialog.setCancelable(false);
+            this.builder.setCancelable(cancelable);
         }
 
-        this.dialog.show();
-
-        Window dialogWindow = this.dialog.getWindow();
+        this.alertDialog = this.builder.create();
+        this.alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button button = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                button.setFocusable(true);
+                button.setFocusableInTouchMode(true);
+                button.requestFocus();
+            }
+        });
+        this.alertDialog.show();
+        Window dialogWindow = this.alertDialog.getWindow();
 
         WindowManager.LayoutParams layoutParams = dialogWindow.getAttributes();
 
@@ -83,8 +96,7 @@ public class ReactNativeAlertModule extends ReactContextBaseJavaModule {
         DisplayMetrics metrics = new DisplayMetrics();
 
         windowManager.getDefaultDisplay().getMetrics(metrics);
-
-        layoutParams.width = (int) (metrics.widthPixels * 0.8);
+        layoutParams.width = (int) (metrics.widthPixels * 0.4);
 
         dialogWindow.setAttributes(layoutParams);
     }
